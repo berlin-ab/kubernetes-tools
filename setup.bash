69652eeacb03e2465314618034fdb674a0f5b7f5
Adam Berlin <aberlin@pivotal.io>
2020-04-14 11:48:53 -0400

    Postgres team setup.

--- setup.bash
+++ setup.bash
@@ -8,7 +8,7 @@ tools_directory=$(dirname $tools_setup_file_path)
 setup_git() {
     # Source git bash helpers.
     . "${tools_directory}/git/git-prompt.sh"
-    . "${tools_directory}/git/git-completion.sh"
+    . "${tools_directory}/git/git-completion.bash"
 
     export GIT_PS1_SHOWDIRTYSTATE=true
     export GIT_PS1_SHOWUPSTREAM="verbose git name"
@@ -21,11 +21,36 @@ setup_git() {
 setup_aliases() {
     alias mini="eval $(minikube docker-env)"
     alias tools="cd ${tools_directory}"
+
+    alias inf='cd ~/workspace/gp-ci-infrastructure'
+    alias ts='cd ~/workspace/gp-toolsmiths/concourse/scripts'
+    alias pgci='cd ~/workspace/postgres-for-kubernetes-ci'
+    alias pg='cd ~/workspace/postgres-for-kubernetes'
+    alias pgp='cd ~/workspace/pg-postgres-release'
+    alias sc='cd ~/workspace/state-checker'
+    alias k='/usr/local/bin/kubectl'    
+}
+
+setup_go() {
+    export GOPATH=$HOME/go
+    export PATH=$PATH:$GOPATH/bin
+}
+
+setup_gcloud() {
+    if [ -f '/Users/adamberlin/workspace/google-cloud-sdk/path.bash.inc' ]; then
+	. '/Users/adamberlin/workspace/google-cloud-sdk/path.bash.inc';
+    fi
+    
+    if [ -f '/Users/adamberlin/workspace/google-cloud-sdk/completion.bash.inc' ]; then
+	. '/Users/adamberlin/workspace/google-cloud-sdk/completion.bash.inc';
+    fi
 }
 
 main() {
     setup_aliases
     setup_git
+    setup_go
+    setup_gcloud
 }
 
 main
